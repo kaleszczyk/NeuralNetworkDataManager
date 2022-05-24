77886fabdad9b93c63911a3eb52cd65b9365e907
kziolkowska <aleszczyk.katarzyna@gmail.com>
2022-05-24 12:21:56 +0200

    Add files via upload

---
+++ ViewModelLocator.cs
@@ -0,0 +1,38 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VI.AOD.NeuralNetworkDataManager.ViewModel;
+
+namespace VI.AOD.NeuralNetworkDataManager
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+            Messenger.Default.Register<NotificationMessage>(this, NotifyUserMethod);
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        private void NotifyUserMethod(NotificationMessage message)
+        {
+            MessageBox.Show(message.Notification);
+        }
+
+    }
+}
